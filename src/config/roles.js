74bbb16f73f823613f300f53ed08b4767c3d8427
piscesfunny <piscesfunny@gmail.com>
2022-07-17 18:49:21 +0100

    added authentication & authorization to routes for uploads

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,6 +1,6 @@
 const allRoles = {
-  user: [],
-  admin: ['getUsers', 'manageUsers'],
+  user: ['manageUploads'],
+  admin: ['manageUploads', 'getUsers', 'manageUsers'],
 };
 
 const roles = Object.keys(allRoles);
--- src/routes/v1/upload.route.js
+++ src/routes/v1/upload.route.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const uploadController = require('../../controllers/upload.controller');
+const auth = require("../../middlewares/auth");
 
 const router = express.Router();
 
-router.post("/initialize-multipart-upload", uploadController.initializeMultipartUpload)
-router.post("/get-multipart-preSigned-urls", uploadController.getMultipartPreSignedUrls)
-router.post("/finalize-multipart-upload", uploadController.finalizeMultipartUpload)
+router.route("/initialize-multipart-upload")
+  .post(auth('manageUploads'), uploadController.initializeMultipartUpload)
+router.route("/get-multipart-preSigned-urls")
+  .post(auth('manageUploads'), uploadController.getMultipartPreSignedUrls)
+router.route("/finalize-multipart-upload")
+  .post(auth('manageUploads'), uploadController.finalizeMultipartUpload)
 
 module.exports = router;
